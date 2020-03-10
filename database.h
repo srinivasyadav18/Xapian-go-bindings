a3a693a0389e2fc9b58a79c8e920a13429aa4c25
srinivasyadav18 <43375352+srinivasyadav18@users.noreply.github.com>
2020-03-11 00:21:56 +0530

    header file

--- database.h
+++ database.h
@@ -1,16 +1,16 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 namespace Gsoc{
-  class Database{
-  private:
-    int id;
-  public:
-    Database();
-    ~Database();
-    void printId();
-    int getId();
-    void setId(int);
-  };
-  void database_func();
+    class Database{
+        private:
+            int id;
+        public:
+            Database();
+            ~Database();
+            void printId();
+            int getId();
+            void setId(int);
+        };
+   void database_func();
 }
 #endif
